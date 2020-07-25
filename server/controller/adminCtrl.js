9e9963b127970a7d6e400b8fda6e5adbd4fca1ad
costel-dev <costelcuconoiu@gmail.com>
2020-07-26 00:50:11 +0200

    added a POST request to add articles

--- server/controller/adminCtrl.js
+++ server/controller/adminCtrl.js
@@ -13,7 +13,7 @@ adminCtrl.handleLogin = async (req, res) => {
           req.session.loggedin = true;
           req.session.username = username;
           req.session.password = password;
-          console.log(`${username} login succesfully!!!`);
+          console.log(`"${username}" login succesfully!!!`);
           res.render("adminDashboard");
         } else {
           console.log("Wrong username or password!");
@@ -30,17 +30,31 @@ adminCtrl.displayRegister = (req, res) => res.render("adminRegister");
 adminCtrl.handleRegister = (req, res) => {
   const { username, email, password, password2 } = req.body;
   const user = { username, email, password };
-  console.log(user);
+  /* console.log(user); */
   let sql = "INSERT INTO admin SET ?";
   if (password === password2) {
     db.query(sql, user, (err, result) => {
       if (err) throw err;
-      console.log(`User ${user.username} added succesfully!!!`);
+      console.log(`User "${user.username}" added succesfully!!!`);
     });
     res.render("adminLogin");
   } else {
     console.log("Password do not match!!!");
   }
+};
+
+adminCtrl.addPost = (req, res) => {
+  const { title, author, content } = req.body;
+  const postDate = new Date().toISOString().slice(0, 19).replace("T", " ");
+  console.log(postDate);
+  const post = { title, author, content, date: postDate };
+  console.log(post);
+  let sql = "INSERT INTO post SET ?";
+  db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    console.log(`Post "${post.title}" added succesfully!!!`);
+  });
+  res.render("adminDashboard");
 };
 
 module.exports = adminCtrl;
--- server/route/adminRoute.js
+++ server/route/adminRoute.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 const adminCtrl = require("../controller/adminCtrl");
 
+//login
 router.get("/login", adminCtrl.displayLogin);
 router.post("/dashboard", adminCtrl.handleLogin);
 //register
 router.get("/register", adminCtrl.displayRegister);
 router.post("/register", adminCtrl.handleRegister);
+
+//article
+router.post("/articles", adminCtrl.addPost);
 
 module.exports = router;
